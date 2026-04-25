ea21085aec7a9c541e67dfac8010ae32fb0f8f75
Heritic22 <h84790104@gmail.com>
2026-04-24 21:23:56 -0500

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: 'static',   // 👈 IMPORTANT
   vite: {
     plugins: [tailwind()],
   },
